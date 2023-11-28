43e5fdeef6b360d9e4f31de5db066402b81d7885
R4C1 <ruffy518@gmail.com>
2023-11-28 05:58:07 -0300

    Estrutura API

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,38 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts 
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+    {  
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            // Essa string de conexão depende da SUA máquina.
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;"
+                            + "Database=ExoApi;Trusted_Connection=True;");
+            
+            // Exemplo 1 de string de conexão:
+            // User
+//ID=sa;Password=admin;Server=localhost;DatabaseGeneratedAttribute=ExoApi;-
+            // + Trusted_Connerction=False;
+
+            // Exemplo 2 de string de conexão:
+            
+//Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=true;
+        }
+    }
+    public DbSet<Projeto> Projetos { get; set;}
+    }
+
+}
+
